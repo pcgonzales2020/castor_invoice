725d98cfc8062b9b055f22e0c4a390e7500fb755
Eduardo <erodriguezp105@gmail.com>
2021-11-24 15:21:16 -0500

    added user service tests

--- src/services/user.service.test.js
+++ src/services/user.service.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-undef */
 const { ObjectId } = require('mongodb');
+const AppError = require('../common/error');
 const UserService = require('./user.service');
 
 const container = {
@@ -32,27 +33,46 @@ const container = {
 describe("User service test", () => {
     const userService = new UserService(container);
 
-    it("should get all users", async () => {
-        const result = await userService.getAll();
+    describe("Get All", () => {
+        it("should get all users", async () => {
+            const result = await userService.getAll();
 
-        expect(result).toEqual([
-            {
-                _id: new ObjectId("507f191e810c19729de860ea"),
+            expect(result).toEqual([
+                {
+                    _id: new ObjectId("507f191e810c19729de860ea"),
+                    username: "cgonzales",
+                    firstName: "christian",
+                    lastName: "gonzales"
+                }
+            ]);
+        });
+    });
+
+    describe("Create", () => {
+        it("should create a successful user", async () => {
+            await userService.create({
                 username: "cgonzales",
                 firstName: "christian",
                 lastName: "gonzales"
+            }, "123456456789");
+
+            expect(container.userRepository.create).toHaveBeenCalled();
+            expect(container.passwordRepository.create).toHaveBeenCalled();
+        });
+
+        it("should throw an ERR_USERNAME_TAKEN error code", async () => {
+            try {
+                await userService.create({
+                    username: "erodriguez",
+                    firstName: "eduardo",
+                    lastName: "rodríguez"
+                }, "123456456789");
+
+                throw new Error("Username must be unique");
+            } catch (error) {
+                expect(error instanceof AppError).toBe(true);
+                expect(error.code).toBe("ERR_USERNAME_TAKEN");
             }
-        ]);
-    });
-
-    it("should create a successful user", async () => {
-        await userService.create({
-            username: "cgonzales",
-            firstName: "christian",
-            lastName: "gonzales"
-        }, "123456456789");
-
-        expect(container.userRepository.create).toHaveBeenCalled();
-        expect(container.passwordRepository.create).toHaveBeenCalled();
+        });
     });
 });
